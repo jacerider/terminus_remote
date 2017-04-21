324dd23b96e483fb638e805ca74e71bdbfb663fa
Cyle Carlson <jacerider@gmail.com>
2017-04-21 02:32:37 -0500

    Another attempt at a fix.

--- commands/create.sh
+++ commands/create.sh
@@ -4,17 +4,16 @@ UPSTREAM_ID=$3
 ORGANIZATION=$4
 
 # Script location
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create log file
 LOG="$DIR/../logs/$MACHINE_NAME.create.bg.log"
 touch $LOG
+
+# Create if necessary
+if [ ! -f $LOG ]; then
+  LOG="../logs/$MACHINE_NAME.create.bg.log"
+fi
 
 terminus site:create $MACHINE_NAME "$LABEL" "$UPSTREAM_ID" --org="$ORGANIZATION" > $LOG 2>&1 &
 PID=$!
--- commands/install.sh
+++ commands/install.sh
@@ -1,17 +1,16 @@
 MACHINE_NAME=$1
 
 # Script location
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create log file
 LOG="$DIR/../logs/$MACHINE_NAME.install.bg.log"
 touch $LOG
+
+# Create if necessary
+if [ ! -f $LOG ]; then
+  LOG="../logs/$MACHINE_NAME.create.bg.log"
+fi
 
 terminus drush "$MACHINE_NAME.dev"  -- site-install --site-name="My Sweetness" -y > $LOG 2>&1 &
 PID=$!
