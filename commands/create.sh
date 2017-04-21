1040e7eb2ab19b0c3d0b5ec99aa25b9caaec6d42
Cyle Carlson <jacerider@gmail.com>
2017-04-21 02:29:18 -0500

    Attempt better bash script location call for Heroku.

--- commands/create.sh
+++ commands/create.sh
@@ -4,7 +4,13 @@ UPSTREAM_ID=$3
 ORGANIZATION=$4
 
 # Script location
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 # Create log file
 LOG="$DIR/../logs/$MACHINE_NAME.create.bg.log"
